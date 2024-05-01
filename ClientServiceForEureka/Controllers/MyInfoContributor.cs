ac55d9c89768224c2a1bc4303b641166b7c7e9e3
SayyehBan <sdvp1991david@gmail.com>
2024-05-01 14:35:49 +0330

    Add project files.

---
+++ ClientServiceForEureka/Controllers/MyInfoContributor.cs
@@ -0,0 +1,16 @@
+﻿using Steeltoe.Management.Info;
+
+namespace ClientServiceForEureka.Controllers;
+
+public class MyInfoContributor : IInfoContributor
+{
+    public void Contribute(IInfoBuilder builder)
+    {
+        builder.WithInfo("MyInfo", new
+        {
+            Description = "میکروسرویس سایه بان",
+            Name = "Order",
+            email = "info@...."
+        });
+    }
+}
